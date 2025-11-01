c7351e4fe71f84abf88a9328585345e43a6dddef
poowamin <james.poowamin7@gmail.com>
2025-11-02 01:52:44 +0700

    add game logic that i dont understand i should deepdrive

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,18 +1,37 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectedSquare, avtivePlayerSymbol }) {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelection(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      // updatedBoard[rowIndex][colIndex] = "X"; // (not recommen)
+      updatedBoard[rowIndex][colIndex] = avtivePlayerSymbol;
+      return updatedBoard;
+    });
+
+    onSelectedSquare();
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelection(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
   const [playerName, setPlayeName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -29,7 +29,7 @@ export default function Player({ initialName, symbol }) {
   }
 
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
